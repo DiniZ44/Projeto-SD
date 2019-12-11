c40efdcdc615a2841934282621915b67c3245af0
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-12-11 10:39:05 -0300

    Questão 06

---
+++ src/br/com/sd/q06/ProdutorConsumidorMOD.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sd.q06;
+
+/**
+ *
+ * @author Hakkinen
+ */
+public class ProdutorConsumidorMOD {
+    
+    public static BoundedBuffer buffer = new BoundedBuffer(100); 
+	
+	public static void main(String[] args) {
+		Runnable produtor = new Runnable() {
+
+			@Override
+			public void run() {
+				for(int i = 0; i < 100000; i++){
+					buffer.push(i);
+				}
+
+			}
+		};
+		Runnable consumidor = new Runnable() {
+
+			@Override
+			public void run() {
+				for(int i = 0; i < 100; i++){
+					System.out.println(buffer.pop());
+				}
+			}
+		};
+
+		new Thread(produtor).start();
+		new Thread(consumidor).start();
+	}
+    
+}
