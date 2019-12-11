05563a581cab522ddef5695c577ad16d7bcbda87
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-12-11 10:38:51 -0300

    Questão 04

---
+++ src/br/com/sd/q04/Contador.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sd.q04;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Hakkinen
+ */
+public class Contador extends Thread{
+    
+    private int valor, limite, threads;
+
+    public Contador(int limite, int threads) {
+        this.limite = limite;
+        this.threads = threads;
+        
+        start();
+    }
+
+    @Override
+    public void run() {
+        while(valor <= limite){
+            try { 
+                sleep(1);
+                System.out.println("Esta Thread de número: "+threads+" Com valor: "+valor);
+                valor++;
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            
+        }
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getLimite() {
+        return limite;
+    }
+
+    public void setLimite(int limite) {
+        this.limite = limite;
+    }
+
+    public int getThreads() {
+        return threads;
+    }
+
+    public void setThreads(int threads) {
+        this.threads = threads;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
