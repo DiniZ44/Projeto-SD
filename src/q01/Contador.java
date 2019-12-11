d550f4db21ec444b566adf49bf49ea35f4598c00
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-12-11 10:15:21 -0300

    Resolução da questão 01

---
+++ src/q01/Contador.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q01;
+
+/**
+ *
+ * @author Hakkinen
+ */
+public class Contador {
+    
+    private static int tempo;
+
+    public Contador(int tempo) {
+        this.tempo = tempo;
+    }
+    
+    public void decrementar (){
+        if(tempo > 0){
+            tempo --;
+        }
+    }
+
+    public static int getTempo() {
+        return tempo;
+    }
+
+    
+}
