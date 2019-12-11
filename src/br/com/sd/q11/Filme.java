76a12af4a3367f465b06d30cb0279d617060cb46
Hakkinen Diniz <santoshakkinen7@gmail.com>
2019-12-11 12:40:58 -0300

    Questão 11

---
+++ src/br/com/sd/q11/Filme.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sd.q11;
+
+/**
+ *
+ * @author Hakkinen
+ */
+public class Filme {
+    
+    String titulo, genero, atores;
+    
+}
